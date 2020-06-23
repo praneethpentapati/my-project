494678a01fea7b6bedab0227265452defc81bc0c
praneethpentapati <praneethpentapati@gmail.com>
2020-06-23 15:14:18 +0530

    Updates

---
+++ precache-manifest.e24bd5598ee70bec814bb14372b698b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3105e4480356ad16ba67fbebe22eb646",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "f15adeb69c14dc22d0d1",
+    "url": "/my-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "330556be7c6a97eee5ec",
+    "url": "/my-project/static/js/2.67d74290.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-project/static/js/2.67d74290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15adeb69c14dc22d0d1",
+    "url": "/my-project/static/js/main.115da092.chunk.js"
+  },
+  {
+    "revision": "cec91190ae77f208eb6f",
+    "url": "/my-project/static/js/runtime-main.d70141e3.js"
+  },
+  {
+    "revision": "ac9b2f70667ba4ddc5f59849f1442c40",
+    "url": "/my-project/static/media/person.ac9b2f70.svg"
+  }
+]);
